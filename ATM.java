c1b6d5067e80556012e8cc87cb1b89b2feee4fce
Shweta7709 <shwetannashte7709@gmail.com>
2025-02-20 10:10:36 +0530

    ATM interface with two methods-one user account

--- ATM.java
+++ ATM.java
@@ -1,10 +1,15 @@
 /*
+
 This is my interface for ATM-here plan to have two methods
 1. getMyBalance()
 2. getCash()
+This is for a single account
+
 */
+
 public interface ATM{
 
 public abstract void getMyBalance();
 public abstract getCash(int amount);
+
 }
